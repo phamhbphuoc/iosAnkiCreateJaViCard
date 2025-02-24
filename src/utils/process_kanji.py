e4525de0a8177a6fa65cb92bf2d68a75af567827
Phuoc <phamhbphuoc@gmail.com>
2025-02-24 15:01:52 +0900

    sorted han viet reading order

--- src/utils/process_kanji.py
+++ src/utils/process_kanji.py
@@ -16,9 +16,19 @@ def get_nghia_han_viet(filtered_result):
 
 def fetch_and_process_kanji(session, query):
 
-    res = call_search_api(session, query, type="kanji", limit=100)
+    res = call_search_api(session, query, type="kanji")
     results = get_results(res)
 
-    am_han_viet_list = [get_am_han_viet(result) for result in results]
+    results_dict = {
+        result.get("kanji"): dict(mean=result.get("mean"), detail=result.get("detail"))
+        for result in results
+        if "kanji" in result
+    }
+
+    sorted_results_list = [results_dict.get(kanji, dict()) for kanji in query]
+
+    am_han_viet_list = [
+        get_am_han_viet(result) for result in sorted_results_list
+    ]
 
     return am_han_viet_list
